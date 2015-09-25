b2890125efb7c9eb189e9f70e5d19fbf5f1ca626
liealdo <aldo@gdplabs.id>
2015-09-25 14:07:21 +0700

    Example 4 -Get web element

--- app/src/androidTest/java/com/example/aldo/robotiumtest/RobotiumTest.java
+++ app/src/androidTest/java/com/example/aldo/robotiumtest/RobotiumTest.java
@@ -88,4 +88,28 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2 {
         solo.assertCurrentActivity("Wrong Activity", DetailActivity.class);
     }
 
+
+    public void testInteractWithWebElement() {
+
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        solo.clickOnButton(1);//Click Button to move to another Detail Activity
+
+        solo.waitForActivity(DetailActivity.class, 2000);// Wait DetailActivity to show with
+        // timeout 2000;
+
+        solo.assertCurrentActivity("Wrong Activity", DetailActivity.class);
+
+        solo.sleep(5000); //Wait 5 seconds to load web
+
+        solo.enterTextInWebElement(By.cssSelector("html>body>input[type='text']"), "testing");
+
+        WebElement webElement = solo.getWebElement(By.id("click"), 0);
+
+        assertTrue(webElement != null);
+
+        solo.clickOnWebElement(By.cssSelector("#click"));
+
+    }
+
 }
