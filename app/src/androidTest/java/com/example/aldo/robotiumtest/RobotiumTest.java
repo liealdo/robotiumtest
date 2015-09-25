04f6c6992397dee2d68dbe83079d7bdc44b1c0c8
liealdo <aldo@gdplabs.id>
2015-09-25 14:01:26 +0700

    Example 1 -Validate Output

--- app/src/androidTest/java/com/example/aldo/robotiumtest/RobotiumTest.java
+++ app/src/androidTest/java/com/example/aldo/robotiumtest/RobotiumTest.java
@@ -26,7 +26,40 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2 {
         super.tearDown();
     }
 
-    public void testRun(){
+    public void testRun() throws Exception{
+
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        assertTrue(solo.searchText("Hello"));
+
+        solo.clickOnButton(0);
+
+        assertTrue("Signed Validation FAIlURE", solo.searchText(solo.getString(R.string
+                .error_message_signed_numeric)));
+
+        solo.enterText(0, "123456");
+
+        solo.clickOnText("Submit");
+
+        assertTrue("Letter Validation FAILURE", solo.searchText(solo.getString(R.string
+                .error_message_signed_alphabet)));
+
+        solo.enterText(1, "asd123");
+
+        solo.clickOnText("Submit");
+
+        assertTrue("Letter Validation FAILURE", solo.searchText(solo.getString(R.string
+                .error_message_signed_alphabet)));
+
+        solo.clearEditText(1);
+
+        solo.enterText(1, "asd");
+
+        solo.clickOnButton(0);
+
+        assertTrue("FAILURE", solo.searchText(getActivity().getString(R.string
+                .success_message)));
+
 
     }
 
